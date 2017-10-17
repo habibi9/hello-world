c543fab42d123f4102a7801a37014915e9fdac29
Habib Derbyshire <habibifier@gmail.com>
2017-10-17 12:30:09 +0800

    Changed GET to POST, able to receive server responses

--- app/views.py
+++ app/views.py
@@ -8,6 +8,11 @@ def hello_world():
 
 @app.route('/receive', methods = ['POST', 'GET'])
 def receive():
+	if request.method == 'POST':
+		name = request.form['name']
+		age = request.form['age']
+		year = str(2017 - int(age))
+		return "Your name is " + name + ", and you were born in " + year
 	if request.method == 'GET':
 		return "This is a message from the server."
 	
